0cb3a9cd00ab3b883e77336c80047cb742465bda
Stephen McDonald <steve@jupo.org>
2017-01-05 15:02:20 +1100

    Whitespace, gitignore

--- server.py
+++ server.py
@@ -31,7 +31,7 @@ class Command(object):
 
     def __call__(self, *args, **kwargs):
         """bind command on first call and call command on subsequent calls"""
-        
+
         if not hasattr(self, "_command"):
             self._command = args[0]
             self.module = self._command.__module__
@@ -41,7 +41,7 @@ class Command(object):
             spec = getargspec(self._command)
             self.args = spec[0][2:]
             self.keywords = spec[-1] is not None
-            self.sig = "%s%s%s" % (self.name, "?" if self.args else "", 
+            self.sig = "%s%s%s" % (self.name, "?" if self.args else "",
                 "&".join(["%s=%s" % (arg, arg) for arg in self.args]))
             for decorator in self._decorators:
                 self._command = decorator(self._command)
@@ -52,9 +52,9 @@ class Command(object):
 
 class CommandResponse(object):
     """provides context for output to a request"""
-    
+
     def __init__(self, request):
-        """retrieves session id from cookie and push it back into the 
+        """retrieves session id from cookie and push it back into the
         cookie after recreating it if session has expired"""
 
         self._request = request
@@ -64,7 +64,7 @@ class CommandResponse(object):
         cookie[name] = request.session["id"]
         cookie[name]["expires"] = 30 * 24 * 60 * 60 # 30 days
         request.id = cookie[name].value
-        self._headers = {"Content-Type": "text/html", 
+        self._headers = {"Content-Type": "text/html",
             "Set-Cookie": cookie.output(header="")}
 
     def write(self, data, status=200):
@@ -85,18 +85,18 @@ class CommandResponse(object):
             return False
         except Exception, e:
             raise e
-    
+
     def render(self, template, **data):
         """respond with template filled with data"""
 
         status = data.pop("status", 200)
-        return self.write(Template(searchList=data, 
-            file=pathjoin(self._request.server.template_dir, template), 
+        return self.write(Template(searchList=data,
+            file=pathjoin(self._request.server.template_dir, template),
             compilerSettings={"useAutocalling": False}), status=status)
 
     def redirect(self, url):
         """send new location header to redirect to"""
-        
+
         request = self._request
         url = list(urlparse(url))
         if not url[0]:
@@ -106,11 +106,11 @@ class CommandResponse(object):
         request.send_response(302)
         request.send_header("Location", urlunparse(url))
         request.end_headers()
-    
+
     def error(self, msg, status=500):
         """render the error template"""
-        
-        self.render("error.html", title="%s Error" % status, 
+
+        self.render("error.html", title="%s Error" % status,
             error=msg, status=status)
 
 
@@ -136,7 +136,7 @@ class CommandRequest(BaseHTTPRequestHandler):
             try:
                 command(self.server, self, **args)
             except Exception, e:
-                self.response.error(format_exc().replace(dirname(__file__) + 
+                self.response.error(format_exc().replace(dirname(__file__) +
                     sep, ""))
         elif exists(pathjoin(self.server.template_dir, "%s.html" % command)):
             # static file
@@ -148,28 +148,28 @@ class CommandRequest(BaseHTTPRequestHandler):
     @property
     def session(self):
         return self.server._sessions[self.id]
-    
+
     def version_string(self):
         return "%s/%s" % (self.server.version_name, __version__)
 
     def log_message(self, *args):
         """pass logging to the server"""
-        
+
         self.server.log_message(*args)
 
 
 class SessionManager(UserDict):
-    
+
     def __init__(self, server):
         """create lock"""
-        
+
         self._lock = RLock()
         self._server = server
         UserDict.__init__(self)
-    
+
     def __getitem__(self, id):
         """create a new session if id doesn't exist"""
-        
+
         with self._lock:
             if id not in self.data:
                 id = str(uuid4())
@@ -177,30 +177,30 @@ class SessionManager(UserDict):
                 self._server.session_start(self.data[id])
             self.data[id]["last"] = time()
             return self.data[id]
-            
+
     def expire(self, timeout, callback):
         """remove session older than timeout and pass them to the callback"""
-        
+
         with self._lock:
-            expired = [session for session in self.data.values() 
+            expired = [session for session in self.data.values()
                 if session["last"] < time() - (timeout * 60.)]
-            self.data = dict([(id, session) for id, session 
+            self.data = dict([(id, session) for id, session
                 in self.data.iteritems() if session not in expired])
             map(callback, expired)
 
 
 class CommandServer(ThreadingMixIn, HTTPServer, Thread):
     """http server with hook for shutting down"""
-    
+
     def __init__(self, host=None, port=None):
         """bind commands"""
-        
+
         self.version_name = self.__class__.__name__
         self.session_timeout = 20
         self.template_dir = pathjoin(dirname(__file__), "templates")
         self._running = True
         self._sessions = SessionManager(self)
-        self._commands = dict([item for item in [(name, eval("self.%s" % name)) 
+        self._commands = dict([item for item in [(name, eval("self.%s" % name))
             for name in dir(self)] if hasattr(item[1], "_command")])
 
         if host is None: host = ""
@@ -210,13 +210,13 @@ class CommandServer(ThreadingMixIn, HTTPServer, Thread):
 
     def run(self):
         """start serving and run until quit is called"""
-        
-        self.address_string = "http://%s:%s" % (self.server_name, 
+
+        self.address_string = "http://%s:%s" % (self.server_name,
             self.server_port)
         while self._running:
             self._sessions.expire(self.session_timeout, self.session_end)
             self.handle_request()
-                
+
     def quit(self):
         """quit serving"""
 
@@ -225,7 +225,7 @@ class CommandServer(ThreadingMixIn, HTTPServer, Thread):
     def log_message(self, *args):
         """request logging - override"""
         pass
-        
+
     def session_start(self, session):
         """session starting - override"""
         pass
@@ -236,16 +236,16 @@ class CommandServer(ThreadingMixIn, HTTPServer, Thread):
 
     @Command()
     def index(self, request, command=""):
-        """display list of commands and docs 
+        """display list of commands and docs
         with links to test forms for commands"""
-        
+
         command = self._commands.get(command, None)
         if command:
             request.response.render("command.html", command=command)
         else:
-            request.response.render("commands.html", 
+            request.response.render("commands.html",
                 commands=self._commands.values())
-                
+
     @Command()
     def source(self, request):
         """display source code for server and sub classes"""
@@ -260,9 +260,9 @@ class CommandServer(ThreadingMixIn, HTTPServer, Thread):
         source.reverse()
         lexer = lexers.get_lexer_by_name("python")
         lexer.add_filter(filters.VisibleWhitespaceFilter(tabs=" ", tabsize=4))
-        request.response.write(highlight("\n\n".join(source), lexer, 
+        request.response.write(highlight("\n\n".join(source), lexer,
             formatters.HtmlFormatter(linenos="inline", full=True)))
-            
+
 
 if __name__ == "__main__":
     from examples.testserver import TestServer
